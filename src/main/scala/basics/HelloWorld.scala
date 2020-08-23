4af9fd0e9e00c32ff60e4f4cf1995ca1a66f1bc1
Mark Lewis <mlewis@trinity.edu>
2020-08-23 17:07:55 -0500

    Updated versions for Fall 2020 and beefed up to HelloWorld to use an RDD to make sure Spark works.

--- src/main/scala/basics/HelloWorld.scala
+++ src/main/scala/basics/HelloWorld.scala
@@ -3,6 +3,8 @@ package basics
 import swiftvis2.plotting.Plot
 import swiftvis2.plotting.styles.ScatterStyle
 import swiftvis2.plotting.renderer.SwingRenderer
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
 
 /**
  * This is here to remind you how to write Scala and to make it so that
@@ -10,8 +12,17 @@ import swiftvis2.plotting.renderer.SwingRenderer
  */
 object HelloWorld {
 	def main(args: Array[String]): Unit = {
-		println("Hello World!")
-		val plot = Plot.simple(ScatterStyle(1 to 10, (1 to 10).map(x => x*x)), "Grade vs. Effort", "Effort Required", "Grade")
+		val conf = new SparkConf().setAppName("Temp Data").setMaster("local[*]")
+  	val sc = new SparkContext(conf)
+  
+		sc.setLogLevel("WARN")
+
+		val rdd = sc.parallelize((1 to 10).map(i => i -> i*i))
+	
+		val data = rdd.collect()
+		val plot = Plot.simple(ScatterStyle(data.map(_._1), data.map(_._2)), "Grade vs. Effort", "Effort Required", "Grade")
 		SwingRenderer(plot, 1000, 1000, true)
+
+		sc.stop()
 	}
 }
