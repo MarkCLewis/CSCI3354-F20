852a3c55659ae87ce475bc33f185fbe5c81f616e
Mark Lewis <mlewis@trinity.edu>
2020-11-18 13:24:17 -0600

    Cleaned up a little in the Perceptron and added an MPL that can learn xor.

--- src/main/scala/perceptron/MLP.scala
+++ src/main/scala/perceptron/MLP.scala
@@ -17,11 +17,9 @@ import scalafx.scene.paint.Color
 import javafx.scene.control.Button
 import scalafx.scene.layout.HBox
 
-case class DataRow(inputs: Array[Double], outputs: Array[Double])
-case class Weights(w: Array[Double])
-
-object Perceptron extends JFXApp {
+object MLP extends JFXApp {
   val inputs = 2
+  val hiddens = 2
   val outputs = 1
   private var eta = 0.01
   private var epoch = 0
@@ -33,16 +31,21 @@ object Perceptron extends JFXApp {
     DataRow(Array(0.0, 1.0), Array(0.0)),
     DataRow(Array(1.0, 1.0), Array(1.0))
   )
-  val weights = ObservableBuffer(
-    Seq.fill(outputs)(
+  val wWeights = ObservableBuffer(
+    Seq.fill(hiddens)(
       Weights(Array.fill(inputs + 1)((math.random - 0.5) * 0.02))
+    ): _*
+  )
+  val vWeights = ObservableBuffer(
+    Seq.fill(outputs)(
+      Weights(Array.fill(hiddens + 1)((math.random - 0.5) * 0.02))
     ): _*
   )
   val canvas = new Canvas(2000, 2000)
   val gc = canvas.graphicsContext2D
 
   stage = new JFXApp.PrimaryStage {
-    title = "Perceptron"
+    title = "MLP"
     scene = new Scene(1200, 1000) {
       val splitPane = new SplitPane
       val addDataButton = new Button("Add Data")
@@ -99,9 +102,9 @@ object Perceptron extends JFXApp {
         _.delegate
       ) ++ resultColumns.map(_.delegate)
       dataTable.editable = true
-      val weightTable = new TableView(weights)
-      val weightColumns = (0 until inputs + 1).map { i =>
-        val col = new TableColumn[Weights, Double](s"Weight ${i}")
+      val wTable = new TableView(wWeights)
+      val wColumns = (0 until inputs + 1).map { i =>
+        val col = new TableColumn[Weights, Double](s"w-Weight ${i}")
         col.cellValueFactory = w => ObjectProperty(w.value.w(i))
         col.cellFactory = TextFieldTableCell.forTableColumn[Weights, Double](
           new StringConverter[Double] {
@@ -110,38 +113,77 @@ object Perceptron extends JFXApp {
           }
         )
         col.onEditCommit = (event) => {
-          weights(weightTable.selectionModel.value.getSelectedIndex()).w(i) =
+          wWeights(wTable.selectionModel.value.getSelectedIndex()).w(i) =
             event.getNewValue()
           dataTable.refresh()
           drawData()
         }
         col
       }
-      weightTable.editable = true
-      weightTable.columns ++= weightColumns.map(_.delegate)
+      wTable.editable = true
+      wTable.columns ++= wColumns.map(_.delegate)
+      val vTable = new TableView(vWeights)
+      val vColumns = (0 until inputs + 1).map { i =>
+        val col = new TableColumn[Weights, Double](s"v-Weight ${i}")
+        col.cellValueFactory = w => ObjectProperty(w.value.w(i))
+        col.cellFactory = TextFieldTableCell.forTableColumn[Weights, Double](
+          new StringConverter[Double] {
+            def fromString(string: String): Double = string.toDouble
+            def toString(t: Double): String = t.toString
+          }
+        )
+        col.onEditCommit = (event) => {
+          vWeights(vTable.selectionModel.value.getSelectedIndex()).w(i) =
+            event.getNewValue()
+          dataTable.refresh()
+          drawData()
+        }
+        col
+      }
+      vTable.editable = true
+      vTable.columns ++= vColumns.map(_.delegate)
       val learnButton = new Button("Learn")
       learnButton.onAction = (ae) => {
         learnOne()
         dataTable.refresh()
-        weightTable.refresh()
+        wTable.refresh()
+        vTable.refresh()
         drawData()
       }
       val learnButton2 = new Button("Learn 10")
       learnButton2.onAction = (ae) => {
         for (_ <- 1 to 10) learnOne()
         dataTable.refresh()
-        weightTable.refresh()
+        wTable.refresh()
+        vTable.refresh()
         drawData()
       }
       val learnButton3 = new Button("Learn 100")
       learnButton3.onAction = (ae) => {
         for (_ <- 1 to 100) learnOne()
         dataTable.refresh()
-        weightTable.refresh()
+        wTable.refresh()
+        vTable.refresh()
         drawData()
       }
-      val learnBox = new HBox(learnButton, learnButton2, learnButton3)
-      val vBox = new VBox(inputBox, dataTable, weightTable, learnBox) //scrollPane1a, scrollPane1b)
+      val learnButton4 = new Button("Learn 1000")
+      learnButton4.onAction = (ae) => {
+        for (_ <- 1 to 1000) learnOne()
+        dataTable.refresh()
+        wTable.refresh()
+        vTable.refresh()
+        drawData()
+      }
+      val learnButton5 = new Button("Learn 10000")
+      learnButton5.onAction = (ae) => {
+        for (_ <- 1 to 10000) learnOne()
+        dataTable.refresh()
+        wTable.refresh()
+        vTable.refresh()
+        drawData()
+      }
+      val learnBox = new HBox(learnButton, learnButton2, learnButton3, learnButton4, learnButton5)
+      val vBox = new VBox(inputBox, dataTable, wTable, vTable, learnBox) //scrollPane1a, scrollPane1b)
       splitPane.items += vBox
       val scrollPane2 = new ScrollPane
       scrollPane2.content = canvas
@@ -153,11 +195,12 @@ object Perceptron extends JFXApp {
 
   def sigmoid(x: Double): Double = 1.0 / (1.0 + math.exp(-x))
 
-  def calcYs(inputs: Seq[Double]): Seq[Double] = {
-    (0 until outputs).map(
-      i => sigmoid((1.0 +: inputs, weights(i).w).zipped.map(_ * _).sum)
-    )
+  def calcZsAndYs(inputs: Seq[Double]): (Seq[Double], Seq[Double]) = {
+    val zs = (0 until hiddens).map(i => sigmoid((1.0 +: inputs, wWeights(i).w).zipped.map(_ * _).sum))
+    zs -> (0 until outputs).map(i => sigmoid((1.0 +: zs, vWeights(i).w).zipped.map(_ * _).sum))
   }
+
+  def calcYs(inputs: Seq[Double]): Seq[Double] = calcZsAndYs(inputs)._2
 
   def learnOne(): Unit = {
     if (learnData.isEmpty) {
@@ -165,10 +208,24 @@ object Perceptron extends JFXApp {
       learnData = ios.toArray
     }
     val index = util.Random.nextInt(learnData.length)
-    val ys = calcYs(learnData(index).inputs)
+    val (zs, ys) = calcZsAndYs(learnData(index).inputs)
     val augmentedInputs = 1.0 +: learnData(index).inputs
-    for (o <- 0 until outputs; i <- 0 until inputs + 1) {
-      weights(o).w(i) += eta * (learnData(index).outputs(o) - ys(o)) * augmentedInputs(i)
+    val augmentedZs = 1.0 +: zs
+    val dv = for (h <- 0 until hiddens+1) yield {
+      for (o <- 0 until outputs) yield {
+        eta * (learnData(index).outputs(o) - ys(o)) * augmentedZs(h)
+      }
+    }
+    val dw = for (i <- 0 until inputs+1) yield {
+      for (h <- 0 until hiddens) yield {
+        eta * (0 until outputs).map(o => (learnData(index).outputs(o) - ys(o)) * vWeights(o).w(h)).sum * zs(h) * (1.0 - zs(h)) * augmentedInputs(i)
+      }
+    }
+    for (h <- 0 until hiddens+1; o <- 0 until outputs) {
+        vWeights(o).w(h) += dv(h)(o)
+    }
+    for (i <- 0 until inputs+1; h <- 0 until hiddens) {
+        wWeights(h).w(i) += dw(i)(h)
     }
     learnData = learnData.patch(index, Nil, 1)
   }
